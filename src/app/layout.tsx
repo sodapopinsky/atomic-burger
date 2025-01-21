69fe4fb73179a9f5588586d8e8d68d4bf51c1ff9
Nicholas <nick@theatomicburger.com>
2025-01-21 10:28:56 -0600

    x

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,43 +1,46 @@
 // app/layout.tsx
 import type { Metadata } from "next";
-import { Jost } from 'next/font/google'
+import { Jost } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/Footer";
-import { Toaster } from "@/components/ui/toaster"
-const jost = Jost({ 
-  subsets: ['latin'],
-  display: 'swap',
-})
+
+const jost = Jost({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Atomic Burger",
-  description: "Atomic Burger is a fast casual restaurant chain that specializes in gourmet burgers, milkshakes, and fries.",
+  description:
+    "Atomic Burger is a fast casual restaurant chain that specializes in gourmet burgers, milkshakes, and fries.",
   icons: {
     icon: [
-      { url: '/favicons/icon.png', type: 'image/png' },
-      { url: '/favicons/favicon.ico' }
+      { url: "/favicons/icon.png", type: "image/png" },
+      { url: "/favicons/favicon.ico" },
     ],
-    apple: [
-      { url: '/favicons/apple-icon.png' }
-    ]
-  }
+    apple: [{ url: "/favicons/apple-icon.png" }],
+  },
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+  },
 };
+
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body className={`${jost.className} relative`}>
-        <div className="overflow-x-hidden">
-          <main className="min-h-screen relative w-full">
-            <Navbar />
+      <body className={`${jost.className}`}>
+        <Navbar />
+        <div className=" overflow-x-hidden">
+          <main className="min-h-[calc(100vh-70px)]">
             {children}
-            <Toaster />
-            <Footer />
           </main>
+          <Footer />
         </div>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Menu from "@/components/menu";
 export default function Home() {
   return (
     <>
-      <HeroSection /> 
+     <HeroSection />
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12 pt-0">
         <Menu />
       </div>
